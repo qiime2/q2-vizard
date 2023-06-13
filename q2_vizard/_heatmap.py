c3b7ebc1bd3a198493f4b4a03f2d6a44d23c87af
Liz Gehret <elizabeth.gehret@gmail.com>
2023-06-13 10:10:29 +0200

    NEW: vizard setup and heatmap placeholder

---
+++ q2_vizard/_heatmap.py
@@ -0,0 +1,13 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2023, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+import pandas as pd
+
+
+def plot_heatmap(output_dir: str, data: pd.DataFrame):
+    pass
---
+++ q2_vizard/plugin_setup.py
@@ -0,0 +1,28 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2023, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from qiime2.plugin import Plugin
+
+from q2_vizard._heatmap import plot_heatmap
+
+plugin = Plugin(name='vizard',
+                version='0.0.1.dev0',
+                website='https://github.com/qiime2/q2-vizard',
+                description='This QIIME 2 plugin is the first choice of wizard'
+                            ' lizards for protection and entertainment.',
+                short_description='The first choice of wizard lizards.')
+
+
+plugin.visualizers.register_function(
+    function=plot_heatmap,
+    inputs={
+    },
+    parameters={},
+    name='Plot Heatmap',
+    description='',
+)
