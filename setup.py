3cb07cd88b95c9ce4bb3e1355512212f88ad1866
Liz Gehret <elizabeth.gehret@gmail.com>
2024-08-21 10:52:19 -0700

    ok i guess we add setup.py back in

---
+++ setup.py
@@ -0,0 +1,16 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2023-2024, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from setuptools import setup
+
+import versioneer
+
+setup(
+    name='q2-vizard',
+    version=versioneer.get_version()
+)
