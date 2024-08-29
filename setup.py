d06e703b21bb90920244cb330f4fb8d24dca5e87
Liz Gehret <elizabeth.gehret@gmail.com>
2024-08-29 13:48:58 -0700

    MAINT: keep pyproject.toml updates in PR until working"
    This reverts commit 41d4819fe9f796645bae60b7661ae9b114db674e, reversing
    changes made to fb7ade1871346ca3a994d468fc707e3f636429d0.

--- setup.py
+++ setup.py
@@ -6,27 +6,10 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 import versioneer
 
 setup(
-    name='q2-vizard',
-    version=versioneer.get_version(),
-    packages=find_packages(),
-    package_data={
-        'q2_vizard': [
-            'assets/*',
-            'assets/heatmap/*',
-            'assets/scatterplot_2d/*'],
-    },
-    author='q2d2',
-    author_email='q2d2@qiime2.org',
-    description='QIIME 2 Plugin used for visualizations.',
-    license='BSD-3-Clause',
-    url='https://github.com/qiime2/q2-vizard',
-    zip_safe=False,
-    entry_points={
-        'qiime2.plugins': ['q2-vizard=q2_vizard.plugin_setup:plugin']
-    }
+    version=versioneer.get_version()
 )
