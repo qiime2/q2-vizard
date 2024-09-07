aee9e6094a2bd4f3e23c09f98c8c17336f083536
Liz Gehret <elizabeth.gehret@gmail.com>
2024-09-06 17:04:17 -0700

    well that was a weird red herring

--- q2_vizard/plugin_setup.py
+++ q2_vizard/plugin_setup.py
@@ -33,9 +33,8 @@ plugin.visualizers.register_function(
         'title': Str
     },
     name='Heatmap',
-    description={
-        'Basic heatmap for visualizing three numeric Metadata measures.'
-    },
+    description='Basic heatmap for visualizing '
+                'three numeric Metadata measures.',
     parameter_descriptions={
         'metadata': 'Any metadata-like input that contains at least three'
                     ' numeric measures for visualizing.',
