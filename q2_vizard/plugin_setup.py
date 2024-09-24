51691847dc19b3eee33fb9627b58c6950de2bf21
Liz Gehret <elizabeth.gehret@gmail.com>
2024-09-24 11:05:38 -0700

    author/description updates

--- q2_vizard/plugin_setup.py
+++ q2_vizard/plugin_setup.py
@@ -18,8 +18,9 @@ plugin = Plugin(name='vizard',
                 website='https://github.com/qiime2/q2-vizard',
                 package='q2_vizard',
                 description='This QIIME 2 plugin is the first choice of wizard'
-                            ' lizards for protection and entertainment.',
-                short_description='The first choice of wizard lizards.')
+                            ' lizards for generalized microbiome data'
+                            ' visualization!',
+                short_description='Generalized microbiome data visualization.')
 
 
 plugin.visualizers.register_function(
