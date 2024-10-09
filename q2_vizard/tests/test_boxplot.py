e6664d920b4f3efbe67d681ae68553010447f26a
Liz Gehret <elizabeth.gehret@gmail.com>
2024-10-09 14:29:53 -0700

    adds all necessary mark types & attrs

--- q2_vizard/tests/test_boxplot.py
+++ q2_vizard/tests/test_boxplot.py
@@ -17,7 +17,7 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from qiime2.plugin.testing import TestPluginBase
 from qiime2 import Metadata
 
-# from q2_vizard import boxplot
+from q2_vizard import boxplot
 
 
 class TestBase(TestPluginBase):
@@ -32,9 +32,23 @@ class TestBase(TestPluginBase):
             ('')
         ]
 
-    def _selenium_boxplot_test(self, driver):
+    def _selenium_boxplot_test(self, driver, distribution_measure, group_by,
+                               whisker_range, box_orientation,
+                               exp_subtitle, exp_legend,
+                               exp_box_marks_len, exp_whisker_marks_len,
+                               exp_whisker_cap_marks_len, exp_median_marks_len,
+                               exp_total_outlier_marks_len,
+                               exp_single_box_outlier_marks_len,
+                               exp_whisker_cap_low_mark,
+                               exp_whisker_cap_high_mark,
+                               exp_median_mark, exp_outlier_mark_id,
+                               exp_outlier_mark_group, exp_outlier_mark):
         with tempfile.TemporaryDirectory() as output_dir:
-            # boxplot()
+            boxplot(
+                output_dir=output_dir, metadata=self.md,
+                distribution_measure=distribution_measure, group_by=group_by,
+                whisker_range=whisker_range, box_orientation=box_orientation
+            )
 
             driver.get(f"file://{os.path.join(output_dir, 'index.html')}")
 
