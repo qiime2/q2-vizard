323f240d84fae5e46b5f4ab1e7f959ed4147e066
Liz Gehret <54517601+lizgehret@users.noreply.github.com>
2026-08-24 13:03:22 +0200

    Update q2_vizard/boxplot.py

--- q2_vizard/boxplot.py
+++ q2_vizard/boxplot.py
@@ -13,8 +13,7 @@ import jinja2
 
 from qiime2 import Metadata, NumericMetadataColumn, CategoricalMetadataColumn
 from ._assets import _copy_vendored_assets
-from ._util import (
-    _json_replace, _measure_validation, _col_type_validation)
+from ._util import _json_replace, _measure_validation, _col_type_validation
 
 
 def boxplot(output_dir: str, metadata: Metadata,
