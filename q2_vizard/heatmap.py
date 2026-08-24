4da9adf186eeb9b7ce81121bbb02704914c737d9
Liz Gehret <54517601+lizgehret@users.noreply.github.com>
2026-08-24 13:03:30 +0200

    Update q2_vizard/heatmap.py

--- q2_vizard/heatmap.py
+++ q2_vizard/heatmap.py
@@ -14,8 +14,7 @@ import importlib
 from qiime2 import Metadata, MetadataColumn, NumericMetadataColumn
 
 from ._assets import _copy_vendored_assets
-from ._util import (
-    _json_replace, _col_type_validation, _measure_validation)
+from ._util import _json_replace, _col_type_validation, _measure_validation
 
 
 def heatmap(output_dir: str, metadata: Metadata,
